b84766a943b3541648dbfa9fb35a3cc8c3357d2f
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2021-03-29 16:30:41 -0300

    parte 2 deu bom. com n=500 nao fica mt bom, com n=5000 fica check

--- exercicio2.py
+++ exercicio2.py
@@ -9,6 +9,50 @@ y0=1.5
 t0=0
 tf=10
 
+# n=5000
+#
+# t=np.linspace(t0,tf,n)
+# x=np.zeros(n)
+# y=np.zeros(n)
+# x[0]=x0
+# y[0]=y0
+#
+# xLinha=np.zeros(n)
+# yLinha=np.zeros(n)
+# xLinha[0]=metodos2.calcXlinha(x0,y0)
+# yLinha[0]=metodos2.calcYlinha(x0,y0)
+#
+# #arrays para guardar resolucao de cada metodo e comparar
+# arraysX=[]
+# arraysY=[]
+#
+# x,y = metodos2.eulerExplicito(x,y,xLinha,yLinha,t)
+# arraysX.append(x)
+# arraysY.append(y)
+#
+# #plot Euler explicito
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+# ax.plot(t, x, label="População de Coelhos", color="blue")
+# ax.plot(t, y, label="População de Raposas", color="red")
+#
+# plt.title("Clássico modelo presa-predador com Euler Explícito")
+# plt.legend(loc="upper left")
+# ax.set(xlabel='Tempo', ylabel='População')
+# plt.show()
+#
+# fig2=plt.figure()
+# ax = fig2.add_subplot(1, 1, 1)
+# ax.plot(x, y, label="Retrato de fase", color="black")
+#
+# plt.title(" Retrato de Fase do Clássico modelo presa-predador com Euler Explícito")
+# plt.legend(loc="upper right")
+# # ax.set(xlabel='Tempo', ylabel='População')
+# plt.show()
+
+
+
+#euler implicito
 n=5000
 
 t=np.linspace(t0,tf,n)
@@ -22,27 +66,29 @@ yLinha=np.zeros(n)
 xLinha[0]=metodos2.calcXlinha(x0,y0)
 yLinha[0]=metodos2.calcYlinha(x0,y0)
 
-#arrays para guardar resolucao de cada metodo e comparar
-arraysX=[]
-arraysY=[]
+x,y = metodos2.eulerImplicito(x,y,xLinha,yLinha,t)
 
-x,y = metodos2.eulerExplicito(x,y,xLinha,yLinha,t)
-arraysX.append(x)
-arraysY.append(y)
-
-#plot Euler explicito
+#plot Euler implicito
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot(t, x, label="População de Coelhos", color="blue")
 ax.plot(t, y, label="População de Raposas", color="red")
 
-plt.title("Clássico modelo presa-predador com Euler Explícito")
-plt.legend(loc="upper right")
+plt.title("Clássico modelo presa-predador com Euler Implicito")
+plt.legend(loc="upper left")
 ax.set(xlabel='Tempo', ylabel='População')
+plt.show()
+
+fig2=plt.figure()
+ax = fig2.add_subplot(1, 1, 1)
+ax.plot(x, y, label="Retrato de fase", color="black")
+
+plt.title(" Retrato de Fase do Clássico modelo presa-predador com Euler Implicito")
+plt.legend(loc="upper right")
+# ax.set(xlabel='Tempo', ylabel='População')
 plt.show()
 
 
 
-# x,y = metodos2.eulerImplicito(x,y,xLinha,yLinha,t)
 
 
--- metodos2.py
+++ metodos2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 lambdaa = 2 / 3
 alfa = 4 / 3
 beta = 1
@@ -24,3 +26,53 @@ def eulerExplicito(x, y, xLinha, yLinha, t):
         xLinha[passo+1] = calcXlinha(x[passo+1],y[passo+1])
         yLinha[passo+1] = calcYlinha(x[passo+1],y[passo+1])
     return x, y
+
+
+def jacobiano(h, x, y):
+    jac = (np.zeros((2,2)))
+
+    jac[0][0] = 1 - h*(lambdaa-alfa*y)
+    jac[0][1] = - h*(alfa*x)
+    jac[1][0] = - h*(beta*y)
+    jac[1][1] = 1 - h*(beta*x-gama)
+    return jac
+
+
+def eulerImplicito(x, y, xLinha, yLinha, t):
+    h=t[1]-t[0]
+
+    eps = 1E-15
+    limiteContador = 20
+    passo=0
+    while (passo < len(t) - 1):
+        # utilizando metodo de newton
+        xl = x[passo] + h * xLinha[passo]
+        yl = y[passo] + h * yLinha[passo]
+        ul=np.array([xl,yl]).reshape(2,1)
+        ulProx=np.array([np.inf,np.inf]).reshape(2,1)
+
+        contador = 0
+        convergiu = False
+        while (contador < limiteContador and not convergiu):
+            # montando o sistema para resolver problema da inversa do jacobiano
+            jacobiandoInvertido = np.linalg.inv(jacobiano(h, ul[0][0], ul[1][0]))
+            vetorDerivada=np.array([calcXlinha(ul[0][0], ul[1][0]),calcYlinha(ul[0][0], ul[1][0])]).reshape(2,1)
+            G = ul-h*vetorDerivada - np.array([x[passo],y[passo]]).reshape(2,1)
+            ulProx = ul - np.dot(jacobiandoInvertido,G)
+            if (np.average(np.abs(ulProx - ul)) < eps):
+                convergiu = True
+            else:
+                ul=ulProx
+            contador += 1
+
+        # appendeando na array x
+        x[passo+1] = ul[0,0]
+        y[passo+1] = ul[1,0]
+
+        # calculando fLinha para proxima iteracao
+        xLinha[passo+1] = calcXlinha(x[passo+1], y[passo+1])
+        yLinha[passo+1] = calcYlinha(x[passo+1], y[passo+1])
+
+        # proximo passo
+        passo += 1
+    return x,y
